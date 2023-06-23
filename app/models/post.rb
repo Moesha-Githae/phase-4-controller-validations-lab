e52f24375c584b8a122aa39620dd9475b2179f2a
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-23 09:32:29 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
-class Post < ApplicationRecord
+lass Post < ApplicationRecord
+  validates :title, presence: true
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validates :content, length: { minimum: 100 }
 end
